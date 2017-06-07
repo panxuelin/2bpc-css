afc1beba6df7cad65e0af540e5cf933854a0765c
panxuelin <panxuelin2006@163.com>
2017-06-07 11:46:21 +0800

    a

---
+++ gulp/hanlders.js
@@ -0,0 +1,22 @@
+var gutil = require('gulp-util');
+var notifier = require('node-notifier');
+
+// Error Handler
+function errorHandler(e) {
+    notifier.notify({
+        title: 'Sass Error',
+        message: e.fileName,
+        sound: true
+    });
+    gutil.log(gutil.colors.red(e.message));
+}
+
+// End Handler
+function endHandler() {
+    gutil.log(gutil.colors.green('Completed!'));
+}
+
+module.exports = {
+    error: errorHandler,
+    end: endHandler
+};
